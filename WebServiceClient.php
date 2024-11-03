ce27dc48a1712731013693974eedfcd00a5c063f
Steve Suehring <steve.suehring@uwsp.edu>
2024-11-03 13:24:25 -0600

    Update WebServiceClient.php
    Updating description, removing example

--- WebServiceClient.php
+++ WebServiceClient.php
@@ -1,8 +1,7 @@
 <?php
 
-/* Woefully incomplete cURL class
- * that can be used as a web service client 
- * for CNMT 310
+/* Basic cURL Class
+ *
  */
 
 class WebServiceClient {
@@ -145,32 +144,5 @@ class WebServiceClient {
   }
 
 } //end class WebServiceClient
-
-/**
-* Example Usage:
-* 
-<?php
-require_once("WebServiceClient.php");
-$url = "http://YOURURL";
-$client = new WebServiceClient($url);
-
-// Default is to POST. If you need to change to a GET, here's how:
-//$client->setMethod("GET");
-
-$apihash = "apiNN";
-$apikey = "8675309";
-$data = array("username" => $username, "password" => $password);
-$action = "authenticate";
-$fields = array("apikey" => $apikey,
-             "apihash" => $apihash,
-              "data" => $data,
-             "action" => $action,
-             );
-$client->setPostFields($fields);
-
-//For Debugging:
-//var_dump($client);
-
-print $client->send();
 
 **/
